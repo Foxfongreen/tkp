cf5ff115135b7d747686a7f5b230850914d4a96d
Foxfonblack <machallua@gmail.com>
2024-10-31 19:05:23 +0200

    31243e55w

---
+++ index.js
@@ -0,0 +1,19 @@
+// const input1 = document.querySelector(".input-1").value;
+// const input2 = document.querySelector(".input-2");
+// const input3 = document.querySelector(".input-3");
+// const a = BigInt("643002406414000010000000096441");
+// const b = BigInt("643002406414000010000000049008");
+// const result = a - b;
+// console.log(result.toString());
+// console.log(input1);
+// function calculateDifference() {
+//     // Получаем значения из input-1 и input-2
+//     const input1 = parseFloat(document.querySelector(".input-1").value) || 0;
+//     const input2 = parseFloat(document.querySelector(".input-2").value) || 0;
+
+//     // Вычисляем разницу
+//     const result = input1 - input2;
+
+//     // Записываем результат в input-3
+//     document.querySelector(".input-3").value = result;
+// }
